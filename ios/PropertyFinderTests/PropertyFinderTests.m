d6aabafdab5264507bb9ffbb26a769fb5fc25721
Felipe Costa <felipe.h.j.costa@gmail.com>
2017-12-11 23:20:11 -0200

    Rename marvelapp to PropertyFinder

--- ios/PropertyFinderTests/PropertyFinderTests.m
+++ ios/PropertyFinderTests/PropertyFinderTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface marvelappTests : XCTestCase
+@interface PropertyFinderTests : XCTestCase
 
 @end
 
-@implementation marvelappTests
+@implementation PropertyFinderTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
