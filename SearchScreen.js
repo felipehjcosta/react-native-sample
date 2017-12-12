1a75e91d8911ae28755ded1075cf02d07c22ec3e
Felipe Costa <felipe.h.j.costa@gmail.com>
2017-12-12 00:41:28 -0200

    Add some style to search screen

--- SearchScreen.js
+++ SearchScreen.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import {
   View,
-  Text
+  StyleSheet,
+  Text,
 } from 'react-native';
+
+var styles = StyleSheet.create({
+  container: {
+    padding: 30,
+    marginTop: 65,
+    alignItems: 'center'
+  },
+  description: {
+    marginBottom: 20,
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565'
+  },
+})
 
 class SearchScreen extends React.Component {
   render() {
     return (
-      <View>
-        <Text>
+      <View style={styles.container}>
+        <Text style={styles.description}>
           Search for houses to buy!
         </Text>
       </View>
