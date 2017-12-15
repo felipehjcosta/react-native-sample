e2cf629dbf2581a4d55bc2cd096495aab1331582
Felipe Costa <felipe.h.j.costa@gmail.com>
2017-12-15 02:31:04 -0200

    Replace App by React Navigation library

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import {
+  StackNavigator,
+} from 'react-navigation';
+
+import SearchScreen from './SearchScreen';
+import SearchResultList from './SearchResultList.js';
+
+const App = StackNavigator({
+  SearchScreen: { screen: SearchScreen },
+  SearchResultList: { screen: SearchResultList },
+});
 
 AppRegistry.registerComponent('PropertyFinder', () => App);
