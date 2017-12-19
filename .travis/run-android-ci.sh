d752e03f626957834f3d5e38abf0723c01f4723e
Felipe Costa <felipe.h.j.costa@gmail.com>
2017-12-19 01:40:36 -0200

    Enable bash enhancement on script for Android on CI

--- .travis/run-android-ci.sh
+++ .travis/run-android-ci.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res
