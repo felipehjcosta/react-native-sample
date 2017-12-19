a1ec39c7b14ee0e13a221443eed6f2219afbd0d2
Felipe Costa <felipe.h.j.costa@gmail.com>
2017-12-19 01:00:27 -0200

    Extract commands from .travis/run-android-ci.sh to .travis.yml

--- .travis/run-android-ci.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res
-detox build --configuration android.emu.release
-detox test --configuration android.emu.release --cleanup
