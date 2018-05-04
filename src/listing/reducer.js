dd96fe2f390213ef60b1e53cfb5854c710497950
Felipe Costa <felipe.h.j.costa@gmail.com>
2018-05-04 12:10:55 -0300

    Separate reducer cases in separate functions

--- src/listing/reducer.js
+++ src/listing/reducer.js
@@ -1,19 +1,23 @@
 // @flow
+
+const loading = (payload) => state => Object.assign(
+  {},
+  payload,
+  state
+)
+
+const fetchDataSuccess = (payload) => state => Object.assign(
+  {},
+  state,
+  payload
+)
+
 export default function reducer (state = {}, action) {
   switch (action.type) {
     case 'ITEMS_IS_LOADING':
-      return Object.assign(
-        {},
-        action.payload,
-        state
-      )
+      return loading(action.payload)(state)
     case 'ITEMS_FETCH_DATA_SUCCESS':
-      return Object.assign(
-        {},
-        state,
-        action.payload
-      )
-
+      return fetchDataSuccess(action.payload)(state)
     default:
       return state
   }
