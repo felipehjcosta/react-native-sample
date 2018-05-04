313b6c3c3cb907fd00bd6e987f3430d709741ff5
Felipe Costa <felipe.h.j.costa@gmail.com>
2018-05-04 12:06:52 -0300

    remove reducer treatment from detail

--- src/detail/reducer.js
+++ src/detail/reducer.js
@@ -1,12 +1,6 @@
 // @flow
 export default function reducer (state = {}, action) {
   switch (action.type) {
-    case 'DETAIL_PROPERTY':
-      return Object.assign(
-        {},
-        state,
-        action.payload
-      )
     default:
       return state
   }
