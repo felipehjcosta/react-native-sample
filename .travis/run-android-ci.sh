c00738fffb6b18b8dd62e96791ad32237f1781a8
Felipe Costa <felipe.h.j.costa@gmail.com>
2018-05-20 17:35:06 -0300

    Enable only detox build script

--- .travis/run-android-ci.sh
+++ .travis/run-android-ci.sh
@@ -3,4 +3,4 @@ set -ev
 
 react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.bundle --assets-dest android/app/src/main/res
 detox build --configuration android.emu.release
-detox test --configuration android.emu.release --cleanup --loglevel verbose
+# detox test --configuration android.emu.release --cleanup --loglevel verbose
